f5f04820af1c19da6b1eeb6d1671e2d8f46fc1aa
rgarcia <voodoorai2000@gmail.com>
2010-11-21 18:11:56 +0100

    facebook connect working

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :facebook_id
   
   has_many :votes
   has_many :voted_proposals, :through => :votes, :source => :proposal
@@ -35,14 +35,16 @@ class User < ActiveRecord::Base
   #   end
      
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-    data = ActiveSupport::JSON.decode(access_token.get('https://graph.facebook.com/me?'))
-    
-    puts "DATA: #{data.to_json}"
+    data = access_token["extra"]["user_hash"]
     
     if user = User.find_by_email(data["email"])
       user
     else # Create an user with a stub password. 
-      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
+      user = User.create!(:email => data["email"], 
+                          :first_name =>  data['first_name'],
+                          :last_name =>  data['last_name'],
+                          :facebook_id => data['id'], 
+                          :password => Devise.friendly_token[0,20])
     end 
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101120193054) do
+ActiveRecord::Schema.define(:version => 20101121170502) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(:version => 20101120193054) do
     t.integer  "represented_users_count"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "facebook_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
